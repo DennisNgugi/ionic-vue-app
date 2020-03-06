7a9c2e34ac9c56720a23afc2c023ab8d8420f51b
DennisNgugi <denongugi73@gmail.com>
2020-03-06 22:54:04 +0300

    Initial commit

---
+++ src/components/ZipSearch.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-grid>
+    <form @submit="onSubmit">
+      <ion-col>
+        <ion-item>
+          <ion-label>Zipcode:</ion-label>
+          <ion-input :value="zip" @input="zip = $event.target.value" placeholder=" US zip code" name="zip"></ion-input>
+        </ion-item>
+      </ion-col>
+          <ion-col>
+            <ion-button type="submit" color="primary" expand="block">Find</ion-button>
+          </ion-col>
+    </form>
+  </ion-grid>
+</template>
+
+<script>
+export default {
+  name:"ZipSearch",
+  data(){
+    return{
+      zip:""
+    }
+  },
+  methods:{
+    onSubmit(e){
+      e.preventDefault()
+      const isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(this.zip);
+      // Test for valid Zipcode
+      if(!isValidZip){
+        this.showAlert();
+      }
+      else{
+        this.$emit("get-zip",this.zip);
+        this.zip="";
+      }
+    },
+    showAlert(){
+      return this.$ionic.alertController
+      .create({
+        header:"Enter zip code",
+        message:"Please enter a valid Us zipcode",
+        buttons:["OK"]
+      }).then(a=>a.present()
+    );
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          ZipInfo
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ZipSearch v-on:get-zip="getZipInfo" />
+    </ion-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ZipSearch from '../components/ZipSearch'
+export default {
+  name: 'Home',
+  components: {
+    ZipSearch
+  },
+  methods:{
+    getZipInfo(zip){
+      console.log(zip)
+    }
+  }
+}
+</script>
